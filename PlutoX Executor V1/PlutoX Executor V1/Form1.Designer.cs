23023be0f90ca49fd427969329800c2089cd5063
GoofyKoofy <goofykoofy34@gmail.com>
2024-11-06 17:00:49 +0000

    Add files via upload

---
+++ PlutoX Executor V1/PlutoX Executor V1/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace PlutoX_Executor_V1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            richTextBox1 = new RichTextBox();
+            label1 = new Label();
+            RobloxOpen = new Label();
+            Status = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            button1.ForeColor = SystemColors.ControlText;
+            button1.Location = new Point(12, 247);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Execute";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            button2.Location = new Point(530, 247);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 1;
+            button2.Text = "Inject";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            button3.Location = new Point(93, 247);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 3;
+            button3.Text = "Kill RBLX";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            button4.Location = new Point(174, 247);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 23);
+            button4.TabIndex = 4;
+            button4.Text = "Clear";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(12, 49);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(593, 192);
+            richTextBox1.TabIndex = 5;
+            richTextBox1.Text = "-- Enter Lua Script Here!";
+            richTextBox1.TextChanged += RichTextBox1_TextChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(269, 37);
+            label1.TabIndex = 6;
+            label1.Text = "PlutoX Executor 1.0";
+            label1.TextAlign = ContentAlignment.TopCenter;
+            label1.Click += label1_Click;
+            // 
+            // RobloxOpen
+            // 
+            RobloxOpen.AutoSize = true;
+            RobloxOpen.Location = new Point(486, 27);
+            RobloxOpen.Name = "RobloxOpen";
+            RobloxOpen.Size = new Size(38, 15);
+            RobloxOpen.TabIndex = 7;
+            RobloxOpen.Text = "label2";
+            // 
+            // Status
+            // 
+            Status.AutoSize = true;
+            Status.Location = new Point(486, 9);
+            Status.Name = "Status";
+            Status.Size = new Size(38, 15);
+            Status.TabIndex = 8;
+            Status.Text = "label3";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Red;
+            BackgroundImageLayout = ImageLayout.None;
+            ClientSize = new Size(617, 282);
+            Controls.Add(Status);
+            Controls.Add(RobloxOpen);
+            Controls.Add(label1);
+            Controls.Add(richTextBox1);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form1";
+            Text = "Pluto Executor";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private RichTextBox richTextBox1;
+        private Label label1;
+        private Label RobloxOpen;
+        private Label Status;
+    }
+}
---
+++ PlutoX Executor V1/PlutoX Executor V1/Form1.cs
@@ -0,0 +1,91 @@
+﻿using FastColoredTextBoxNS;
+using ForlornApi;
+
+namespace PlutoX_Executor_V1
+{
+    public partial class Form1 : Form
+    {
+        System.Windows.Forms.Timer time = new System.Windows.Forms.Timer();
+        public Point mouseLocation;
+        public Form1()
+        {
+            // Enable form dragging
+            this.MouseDown += new MouseEventHandler(Form_MouseDown);
+            this.MouseMove += new MouseEventHandler(Form_MouseMove);
+
+            time.Tick += timerTick;
+            time.Start();
+
+            InitializeComponent();
+        }
+
+        private void Form_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseLocation = new Point(-e.X, -e.Y);
+        }
+        // Handle mouse move to drag the form
+
+        private void Form_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Point mousePos = Control.MousePosition;
+                mousePos.Offset(mouseLocation.X, mouseLocation.Y);
+                this.Location = mousePos;
+            }
+        }
+
+        private void timerTick(object sender, EventArgs e)
+        {
+            if (ForlornApi.Api.IsInjected())
+            {
+                Status.Text = "STATUS: INJECTED";
+            }
+            else
+            {
+                Status.Text = "STATUS: NOT INJECTED";
+            }
+
+            if (ForlornApi.Api.IsRobloxOpen())
+            {
+                RobloxOpen.Text = "Roblox Open: ✅";
+                RobloxOpen.ForeColor = Color.Black;
+            }
+            else
+            {
+                RobloxOpen.Text = "Roblox Open: ❌";
+                RobloxOpen.ForeColor = Color.Black;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ForlornApi.Api.ExecuteScript(richTextBox1.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ForlornApi.Api.Inject();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ForlornApi.Api.KillRoblox();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RichTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            richTextBox1.LanguageOption = (RichTextBoxLanguageOptions)Language.Lua;
+        }
+    }
+}
